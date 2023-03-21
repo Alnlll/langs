94afc5a7fe0c4044ba2cf51bd7ad46daacc5c209
xinlia <xinlia@qti.qualcomm.com>
2023-03-21 21:18:03 +0800

    [rust] use gzbuilder to compress.

--- rust/50_prjs/compression_001/src/main.rs
+++ rust/50_prjs/compression_001/src/main.rs
@@ -4,9 +4,11 @@ use structopt::StructOpt;
 extern crate flate2;
 use flate2::Compression;
 // use flate2::write::ZlibEncoder;
-use flate2::write::GzEncoder;
+// use flate2::write::GzEncoder;
+use flate2::GzBuilder;
 
-use std::io::{ BufReader, BufWriter };
+use std::io::Write;
+use std::io::{ BufReader, BufWriter, Read };
 use std::fs::File;
 
 #[derive(StructOpt, Debug)]
@@ -14,30 +16,33 @@ use std::fs::File;
 struct Options {
     #[structopt(short = "s", long = "src", parse(from_os_str))]
     /// The file path to compress
-    src_file : Option<std::path::PathBuf>,
+    src_file : std::path::PathBuf,
 
     #[structopt(short = "t", long = "tgt", parse(from_os_str))]
     /// The file path to compressed
-    tgt_file : Option<std::path::PathBuf>,
+    tgt_file : std::path::PathBuf,
 }
 
 fn main() -> Result<(), std::io::Error> {
     let options = Options::from_args();
 
-    let src_path = options.src_file;
-    let tgt_path = options.tgt_file;
+    // let src_path = options.src_file;
+    // let tgt_path = options.tgt_file;
 
-    let src_file = File::open(&src_path.unwrap())?;
-    let tgt_file = File::create(&tgt_path.unwrap())?;
+    // let src_file = File::open(&src_path.unwrap())?;
+    // let tgt_file = File::create(&tgt_path.unwrap())?;
 
-    let mut reader = BufReader::new(&src_file);
-    let writer = BufWriter::new(&tgt_file);
+    // let mut reader = BufReader::new(&src_file);
+    // let writer = BufWriter::new(&tgt_file);
 
     // let mut encoder = ZlibEncoder::new(writer, Compression::default());
     // std::io::copy(&mut reader, &mut encoder)?;
-    let mut gz = GzEncoder::new(writer, Compression::default());
-    std::io::copy(&mut reader, &mut gz)?;
-    gz.finish()?;
+    /* let mut gz = GzEncoder::new(writer, Compression::default()); */
+    /* std::io::copy(&mut reader, &mut gz)?; */
+    // let compressed = gz.finish()?;
+
+    // println!("input len : {:?}", src_file.metadata().unwrap().len());
+    // println!("output len: {:?}", compressed.get_ref().metadata().unwrap().len());
     // let mut input_buf = BufReader::new(&File::open(&options.src_file));
     // let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
     // e.write_all(b"foo");
@@ -46,5 +51,25 @@ fn main() -> Result<(), std::io::Error> {
 
     // println!("{:?}", compressed_bytes.unwrap());
     //
+    
+    // read input file
+    let mut input_file = File::open(&options.src_file)?;
+    let mut input_data = Vec::new();
+    input_file.read_to_end(&mut input_data)?;
+
+    // compress the file
+    let mut encoder = GzBuilder::new()
+                        .filename(options.src_file.file_name().unwrap().to_str().unwrap())
+                        .write(File::create(&options.tgt_file)?, Compression::default());
+    encoder.write_all(&input_data)?;
+    encoder.finish()?;
+
+    // print len info
+    let input_len = input_data.len();
+    let output_len = std::fs::metadata(options.tgt_file)?.len();
+
+    println!("input_len: {:?}", input_len);
+    println!("output_len: {:?}", output_len);
+
     return Ok(());
 }
